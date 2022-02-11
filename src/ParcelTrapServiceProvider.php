47ae4cdeb0da47947ce7307f18aac706bf09ac26
Owen Voke <development@voke.dev>
2022-02-11 16:36:56 +0000

    fix: enforce config array shape correctly

--- src/ParcelTrapServiceProvider.php
+++ src/ParcelTrapServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace ParcelTrap\Laravel;
 
 use InvalidArgumentException;
+use ParcelTrap\Contracts\Driver;
 use ParcelTrap\ParcelTrap;
-use ParcelTrap\ParcelTrap\Contracts\Driver;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -22,17 +22,15 @@ class ParcelTrapServiceProvider extends PackageServiceProvider
     public function packageRegistered(): void
     {
         $this->app->singleton(ParcelTrap::class, function ($app) {
-            /** @var array{default: string, drivers: array<string, array<string, mixed>>} $config */
+            /** @var array{default?: string|null, drivers?: array<string, array{driver?: class-string<Driver>}>} $config */
             $config = $app['config']['parceltrap'] ?? [];
 
             return tap(ParcelTrap::make(), static function (ParcelTrap $client) use ($config) {
-                if (! isset($config['default']) || $config['default'] === null) {
+                if (! isset($config['default']) || $config['default'] === '') {
                     throw new InvalidArgumentException('A default driver must be specified in the configuration');
                 }
 
-                // @phpstan-ignore-next-line
                 collect($config['drivers'] ?? [])->each(
-                    /** @param array{driver: class-string<Driver>} $driverConfig */
                     function (array $driverConfig, string $driverName) use ($client) {
                         if (! isset($driverConfig['driver']) || ! class_exists($driverConfig['driver'])) {
                             throw new InvalidArgumentException('A valid driver class must be specified in the configuration');
